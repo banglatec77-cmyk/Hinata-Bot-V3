3c2dfbe27e5bad67f56994e47884dc4d131ba433
banglatec77-cmyk <banglatec77@gmail.com>
2026-09-07 11:48:25 +0600

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -9,7 +9,7 @@ module.exports = {
         config: {
                 name: "ai",
                 version: "1.7",
-                author: "MahMUD",
+                author: "Xinato",
                 countDown: 5,
                 role: 0,
                 description: {
